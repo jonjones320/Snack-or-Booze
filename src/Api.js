0cd1a31b0d6612dc29d00b53067bdc5d18ac4717
JJ <jonjones320@gmail.com>
2024-07-29 00:53:37 -0700

    Added 'getDrinks' server request.

--- src/Api.js
+++ src/Api.js
@@ -14,7 +14,12 @@ class SnackOrBoozeApi {
   static async getSnacks() {
     const result = await axios.get(`${BASE_API_URL}/snacks`);
     return result.data;
-  }
+  };
+
+  static async getDrinks() {
+    const result = await axios.get(`${BASE_API_URL}/drinks`);
+    return result.data;
+  };
 
 }
 
